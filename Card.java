46e548b55af8f6aa2fe98c317d4a9f1a2b76e8c3
MoyMoy108 <61925747+MoyMoy108@users.noreply.github.com>
2020-03-08 15:27:38 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,7 +1,13 @@
 package FinalProject;
 
-import java.applet.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 
 public class Card {
@@ -11,79 +17,111 @@ public class Card {
 	private int value;
 	private String[] ranks = {"Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
 	private String[] suits = {"Clubs","Diamonds","Hearts","Spades"};
-	image clubs = new image[13];
-        image hearts = new image[13];
-        image spades = new image[13];
-        image diamonds = new image[13];
-	
+	ArrayList<Image> clubs = new ArrayList<Image>();
+	ArrayList<Image> diamonds = new ArrayList<Image>();
+	ArrayList<Image> hearts = new ArrayList<Image>();
+	ArrayList<Image> spades = new ArrayList<Image>();
+
+	BufferedImage brd =null;
+
 	//constructor for cards
-	Card(int suit, int values) {
-	    this.rank = values;
-	    this.suit = suit;
-		
-		//if statement to determine the suit (1,2,3,4)
-		for( int i = 0; i <= 13; i++ ) {
-   			clubs[i] = getImage( getDocumentBase(), c(i).gif )
-		}
-		
-		for( int i = 0; i <= 13; i++ ) {
-   			hearts[i] = getImage( getDocumentBase(), h(i).gif )
-		}
-		
-		for( int i = 0; i <= 13; i++ ) {
-   			spades[i] = getImage( getDocumentBase(), s(i).gif )
-		}
-		
-		for( int i = 0; i <= 13; i++ ) {
-   			diamonds[i] = getImage( getDocumentBase(), d(i).gif )
-		}
-        
+	Card(int suit, int values) throws IOException  {
+		this.rank = values;
+		this.suit = suit;
 	}
-	
+
+
 	//gets the rank
 	public int getRank()
 	{
-	    return rank;
+		return rank;
 	}
-	
+
 	//gets the suit
 	public int getSuit()
 	{
-	    return suit;
+		return suit;
 	}
 
 	//gets the values based on the rank of the card
 	public int getValue()
 	{	
 		//based on blackjack facecards are worth 10
-	    if(rank>10)
-	    {
-	        value=10;
-	    }
-	    //aces are worth 11
-	    else if(rank==1)
-	    {
-	        value=11;
-	    }
-	    else
-	    {
-	        value=rank;
-	    }
-	    return value;
+		if(rank>10)
+		{
+			value=10;
+		}
+		//aces are worth 11
+		else if(rank==1)
+		{
+			value=11;
+		}
+		else
+		{
+			value=rank;
+		}
+		return value;
 	}
-	
+
 	/*
 	 * Returns the string version of a card.
 	 */
-	
+
 	public String toString()
 	{
-	    return ranks[rank]+" of "+suits[suit];
+		return ranks[rank]+" of "+suits[suit];
 	}
-	
+
 	//sets the value of a card
 	public void setValue(int set)
 	{
-	    value = set;
+		value = set;
 	}
+
+
+
+	public Collection<? extends Image> MyImage(String filename) throws IOException{
+	//Construct the fully qualified path to filename
+	String path = System.getProperty("user.dir");
+	path += File.separator + "Files" + File.separator;
+	path += filename;
+
+	try {
+
+		File file = new File(path);	
+
+
+		brd = ImageIO.read(file) ;
+	} catch (FileNotFoundException e) {
+		
+		// TODO Auto-generated catch block
+		System.out.println("File not Found " + path);
+	}
+
+
+	//read lines from the file
+
+
+
+	//if statement to determine the suit (1,2,3,4)
+	for( int i = 0; i < 13; i++ ) {
+		clubs.addAll(MyImage( (i+1) + "C"+".jpg" )) ;
+	}
+
+	for( int i = 0; i < 13; i++ ) {
+		
+		hearts.addAll(MyImage( (i +1)+"H" + ".jpg" )) ;
+	}
+
+	for( int i = 0; i < 13; i++ ) {
+		spades.addAll(MyImage( (i+1)+"S" +  ".jpg" ));
+	}
+
+	for( int i = 0; i < 13; i++ ) {
+		diamonds.addAll(MyImage((i+1) + "D" + ".jpg" ));
+	}
+	return null;
+
+}
+
 }
