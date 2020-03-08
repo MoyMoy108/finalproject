0dbd6979142170690e1b6a836a4aaeb0d6752cee
Saigopal01 <56892816+Saigopal01@users.noreply.github.com>
2020-03-08 17:07:23 -0600

    Update ComponentScreen.java

--- ComponentScreen.java
+++ ComponentScreen.java
@@ -1,6 +1,9 @@
 package FinalProject;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,15 +15,17 @@ import javax.swing.JLabel;
 public class ComponentScreen extends JComponent implements ActionListener{
 	
 	JFrame frame = new JFrame();
+	Boolean hit_button = false;
+	Boolean stay_button = false;
+	
+	Rectangle yourcards_rect = new Rectangle(100, 400, 200, 20);
 
 	public void display() {
 
 		this.addFrame();
-		this.addHitButton();
-		this.addStayButton();
-		this.addYouLabel();
-		this.addDealerLabel();
-
+		
+		
+		
 	}
 
 
@@ -30,46 +35,65 @@ public class ComponentScreen extends JComponent implements ActionListener{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(0, 0, 1000, 1000);
 		frame.setBackground(Color.GREEN);
-		//frame set resize = false
+		frame.setResizable(false);
+		this.addHitButton();
+		this.addStayButton();
+		this.addYouLabel();
+		this.addDealerLabel();
+
 		frame.setVisible(true);
 	}
 
 
 	public void addHitButton() {
-		JButton start_button = new JButton("HIT!");  
-		start_button.setBounds(100, 100, 100, 100);
-		start_button.setBackground(Color.GRAY);
-		frame.add(start_button);
+		JButton hit_button = new JButton("HIT!");  
+		hit_button.setBounds(100, 450, 100, 100);
+		hit_button.setBackground(Color.GRAY);
+		frame.add(hit_button);
 
-		start_button.addActionListener(this);
+		hit_button.addActionListener(this);
 	}
 	
 	public void addStayButton() {
-		JButton start_button = new JButton("STAY!");  
-		start_button.setBounds(200, 100, 100, 100);
-		start_button.setBackground(Color.GRAY);
-		frame.add(start_button);
+		JButton stay_button = new JButton("STAY!");  
+		stay_button.setBounds(200, 450, 100, 100);
+		stay_button.setBackground(Color.GRAY);
+		frame.add(stay_button);
+		
 
-		start_button.addActionListener(this);
+		stay_button.addActionListener(this);
 	}
 
 
 	public void addYouLabel() {
-		JLabel welcome = new JLabel("YOU");
-		welcome.setBounds(100, 500, 200, 20);
-		frame.add(welcome);
+		JLabel your_cards = new JLabel("YOUR CARDS");
+		your_cards.setBounds(100, 400, 200, 20);
+		
+		your_cards.setBackground(Color.red);
+		frame.add(your_cards);
 	}
 	
 	public void addDealerLabel() {
-		JLabel welcome = new JLabel("DEALER");
-		welcome.setBounds(100, 50, 200, 20);
-		frame.add(welcome);
+		JLabel dealer = new JLabel("DEALER");
+		Rectangle dealer_rect = new Rectangle(100, 25, 200, 20);
+		dealer.setBounds(dealer_rect);
+		//dealer_rect.(Color.red);
+		frame.add(dealer);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e2) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		Graphics2D g2 = (Graphics2D)g;
+		g2.draw(yourcards_rect);
+		g2.setColor(Color.RED);
+		g2.fill(yourcards_rect);
+		g2.drawString("Please choose an empty square // See console for winner", 100, 450);
 	}
 
 }
