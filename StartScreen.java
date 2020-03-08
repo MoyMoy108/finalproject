65482fc3a6e28b5c144c94f05245accdf9d8321e
Saigopal01 <56892816+Saigopal01@users.noreply.github.com>
2020-03-07 21:55:44 -0700

    Update StartScreen.java

--- StartScreen.java
+++ StartScreen.java
@@ -1,38 +1,73 @@
 package FinalProject;
 
-import javax.swing.JComponent;
-
-
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Rectangle;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JPanel;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JButton;  
-
 public class StartScreen extends JComponent implements ActionListener{
 	
+	JFrame frame = new JFrame();
+	InstructionScreen screen2 = new InstructionScreen();
 	
-	public void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D)g;
-		g2.drawString("Welcome to BlackJack", 300, 200);
-
-	
+	public void display() {
+		
+	this.addFrame();
+	this.addStartButton();
+	this.addStartLabel();
+		
 	}
 	
+	
+	public void addFrame() {
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(0, 0, 1000, 1000);
+		frame.setBackground(Color.GREEN);
+		frame.setVisible(true);
+	}
+	
+	
+	public void addStartButton() {
+		JButton start_button = new JButton("Start Game");  
+		start_button.setBounds(100, 100, 100, 100);
+		start_button.setBackground(Color.RED);
+		frame.add(start_button);
+
+		start_button.addActionListener(this);
+	}
+	
+	
+	public void addStartLabel() {
+		JLabel welcome = new JLabel("Welcome to Black Jack");
+		welcome.setBounds(100, 70, 200, 20);
+		frame.add(welcome);
+	}
 	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub	
-		
+	frame.setVisible(false);
+	screen2.display();
 	}
+	
+	
+
+	
+	
+	
+//	
+//	public void paintComponent(Graphics g) {
+//		Graphics2D g2 = (Graphics2D)g;
+//		g2.drawString("Welcome to BlackJack", 300, 200);
+//		g2.setBackground(Color.GREEN);
+//		frame.print(g2);
+//	
+//	}
+
+	
+	
 	
 	
 	
