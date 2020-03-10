f961f80802f0d09e117834463531b3e8377b64b3
MoyMoy108 <61925747+MoyMoy108@users.noreply.github.com>
2020-03-09 19:07:03 -0600

    Update NewInstructionScreen.java

--- NewInstructionScreen.java
+++ NewInstructionScreen.java
@@ -2,20 +2,23 @@ package FinalProject;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Color;
-import javax.swing.UIManager;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
-public class NewStartScreen extends JFrame implements ActionListener{
-static NewStartScreen frame;
+public class NewInstructionScreen extends JFrame implements ActionListener {
+
+	static NewInstructionScreen frame;
 	private JPanel contentPane;
 
 	/**
@@ -25,7 +28,7 @@ static NewStartScreen frame;
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					frame = new NewStartScreen();
+					frame = new NewInstructionScreen();
 					frame.setResizable(false);
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -38,8 +41,7 @@ static NewStartScreen frame;
 	/**
 	 * Create the frame.
 	 */
-	public NewStartScreen() {
-		setTitle("Black Jack");
+	public NewInstructionScreen() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -48,27 +50,29 @@ static NewStartScreen frame;
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Start");
-		btnNewButton.setBackground(Color.GRAY);
-		btnNewButton.setBounds(165, 106, 89, 23);
+		JLabel lblNewLabel = new JLabel("INSTRUCTIONS");
+		lblNewLabel.setBackground(new Color(0, 102, 51));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(154, 11, 119, 44);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Let's Play!");
+		btnNewButton.setBackground(Color.RED);
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton.setBounds(154, 202, 119, 34);
 		btnNewButton.addActionListener(this);
 		contentPane.add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("Welcome to Black Jack!");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(136, 49, 147, 46);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("By Moises and Saigopal");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(136, 185, 147, 14);
-		contentPane.add(lblNewLabel_1);
+		JTextArea txtrWelcomeToBlack = new JTextArea();
+		txtrWelcomeToBlack.setText("Welcome to Black Jack!\r\n\r\nHere are the rules!!!");
+		txtrWelcomeToBlack.setBounds(38, 48, 361, 148);
+		contentPane.add(txtrWelcomeToBlack);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		NewInstructionScreen.main(null);
 		frame.setVisible(false);
+		FinalGame.main(null);
 	}
 }
