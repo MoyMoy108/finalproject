535ff7bc950d080308ba8a2666d4b1e8509fd5bd
Saigopal01 <56892816+Saigopal01@users.noreply.github.com>
2020-03-10 15:36:13 -0600

    Update Gameplay.java

--- Gameplay.java
+++ Gameplay.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Gameplay {
+	//declaring class level variables
 	//card related variables
+	
 	ArrayList<Card> dealer_hand;
 	ArrayList<Card> player_hand;
 	Card[] dealer_cards;
@@ -19,125 +21,62 @@ public class Gameplay {
 	private String winner;
 	FinalGame finalgame;
 	Deck deck;
-	//ImagePanel img;
+	
+	public int player_card_display_counter = 0;
+	public int dealer_card_display_counter = 0;
 
 	/*
-	 * A gameplay constructor
+	 * A game play constructor
 	 */
+	
 	Gameplay(Deck deck, FinalGame finalgame){
 		this.finalgame = finalgame;
 		this.deck = deck;
 		deck.shuffle();
-		//this.img = img;
 		playermoney =1000;
 		dealer_hand = new ArrayList<>();
 		player_hand = new ArrayList<>();
+		
 		//dealer gets two cards to start according to the rules
 		for(int i = 0; i<2; i++)
 		{	
 			Card c = deck.drawCard();
 			System.out.println("Dealer " + c);
 			dealer_hand.add(c);
+			if (i == 0) {
+				finalgame.setPanel1(c);
+			}
+			else if (i ==1) {
+			finalgame.setPanel2(c);
+			}
 		}
+		
 		//gives the player one card to start with
 		Card p = deck.drawCard();
-		finalgame.setPanel_6(p);
+		finalgame.setPanel8(p);
 		System.out.println("Player " + p);
 		player_hand.add(p);
-		//converts the arraylists into arrays
+		
+		//converts the array lists into arrays
 		dealer_cards =  dealer_hand.toArray(new Card[0]);
 		player_cards = player_hand.toArray(new Card[0]);
+		
 		//gets the current value of the dealer's hand upon starting
 		for(int i = 0; i<dealer_cards.length; i++)
 		{
 			dealerhandvalue += dealer_cards[i].getValue();
 		}
+		
 		//gets the value of the users hand upon starting
 		for(int i = 0; i< player_cards.length; i++)
 		{
 			playerhandvalue += player_cards[i].getValue();
 		}
-
 	}
 
-	/*
-	 * Player plays
-	 */
-	public void playerrun(Deck deck) {
-		while(playerCanHit()) {
-			playerHit(deck);
-		}
-	}
-
-	/*
-	 * gets the player's money
-	 */
-	public double getplayermoney() {
-		return playermoney;
-	}
-
-	public ArrayList<Card> getPlayerHand(){
-		return player_hand;
-	}
-	/*
-	 * way of taking a bet and finding out if it is more than
-	 * current assets
-	 */
-	public void bet() {
-		Scanner bet = new Scanner(System.in);
-		int currentbet= bet.nextInt();
-		if( currentbet > playermoney) {
-			//inform user that that doesn't work 
-			//pop up possibly
-			System.out.println("That's money you don't have");
-			String set = "That's money you don't have";
-			finalgame.setS(set);
-			finalgame.addMessage();
-
-		}
-		betamount = currentbet;
-		playermoney -= betamount; 
-	}
-
-	/*
-	 * check what the amount is for bet
-	 */
-	public double getBet() {
-		return betamount;
-	}
-
-	/*
-	 * function that will get the dealer to hit 
-	 * adds card to dealer's hand
-	 * calculates what their current hand is worth
-	 */
-	public void dealerHit(Deck deck)
-	{
-		dealerhandvalue = 0;
-		Card d = deck.drawCard();
-		System.out.println("dealer gets: " + d);
-		dealer_hand.add(d);
-		dealer_cards = dealer_hand.toArray(new Card[0]);
-		for(int i = 0; i< dealer_cards.length; i++)
-		{
-			dealerhandvalue += dealer_cards[i].getValue();
-		}
-		this.checkDealerBust();
-	}
-
-	/*
-	 * make sure that the value is under 17 
-	 * before the next hit
-	 */
-	public boolean dealerCanHit()
-	{
-		if(dealerhandvalue<17)
-		{
-			return true;
-		}
-		return false;
-	}
-
+	//PLAYER STUFF
+	
+	
 	/*
 	 * allows the user to hit
 	 * gives them another card
@@ -152,6 +91,30 @@ public class Gameplay {
 			System.out.println("Player Hit: " + c);
 			player_hand.add(c);
 			player_cards = player_hand.toArray(new Card[0]);
+			player_card_display_counter++;
+			
+			
+			if(player_card_display_counter == 1){
+				finalgame.setPanel9(c);
+			}
+			else if(player_card_display_counter ==2) {
+				finalgame.setPanel10(c);
+			}
+			else if(player_card_display_counter ==3) {
+				finalgame.setPanel11(c);
+			}
+			else if(player_card_display_counter ==4) {
+				finalgame.setPanel12(c);
+			}
+			else if(player_card_display_counter ==5) {
+				finalgame.setPanel13(c);
+			}
+			else if(player_card_display_counter ==6) {
+				finalgame.setPanel14(c);
+			}
+			else {}
+			
+			
 			for(int i = 0; i< player_cards.length; i++)
 			{
 				playerhandvalue += player_cards[i].getValue();
@@ -178,6 +141,7 @@ public class Gameplay {
 
 	}
 
+	
 	/*
 	 * checks if player can continue hitting
 	 * if below 21 then they can
@@ -191,6 +155,7 @@ public class Gameplay {
 		return false;
 	}
 
+	
 	/*
 	 * states whether or not the user has decided to stay
 	 */
@@ -199,6 +164,119 @@ public class Gameplay {
 		return true;
 	}
 
+
+	/*
+	 * Player plays
+	 */
+	public void playerrun(Deck deck) {
+		while(playerCanHit()) {
+			playerHit(deck);
+		}
+	}
+	
+
+	/*
+	 * gets the player's money
+	 */
+	public double getplayermoney() {
+		return playermoney;
+	}
+
+	public ArrayList<Card> getPlayerHand(){
+		return player_hand;
+	}
+	
+	
+	/*
+	 * way of taking a bet and finding out if it is more than
+	 * current assets
+	 */
+	public void bet() {
+		Scanner bet = new Scanner(System.in);
+		int currentbet= bet.nextInt();
+		if( currentbet > playermoney) {
+			//inform user that that doesn't work 
+			//pop up possibly
+			System.out.println("That's money you don't have");
+			String set = "That's money you don't have";
+			finalgame.setS(set);
+			finalgame.addMessage();
+
+		}
+		betamount = currentbet;
+		playermoney -= betamount; 
+	}
+
+	
+	/*
+	 * check what the amount is for bet
+	 */
+	public double getBet() {
+		return betamount;
+	}
+
+	//DEALER STUFF
+	
+	/*
+	 * function that will get the dealer to hit 
+	 * adds card to dealer's hand
+	 * calculates what their current hand is worth
+	 */
+	public void dealerHit(Deck deck)
+	{
+		dealerhandvalue = 0;
+		Card d = deck.drawCard();
+		System.out.println("dealer gets: " + d);
+		dealer_hand.add(d);
+		dealer_cards = dealer_hand.toArray(new Card[0]);
+		dealer_card_display_counter++;
+		if(dealer_card_display_counter == 1){
+			finalgame.setPanel3(d);
+		}
+		else if(dealer_card_display_counter ==2) {
+			finalgame.setPanel4(d);
+		}
+		else if(dealer_card_display_counter ==3) {
+			finalgame.setPanel5(d);
+		}
+		else if(dealer_card_display_counter ==4) {
+			finalgame.setPanel6(d);
+		}
+		else if(dealer_card_display_counter ==5) {
+			finalgame.setPanel7(d);
+		}
+		else {}		
+		
+		for(int i = 0; i< dealer_cards.length; i++)
+		{
+			dealerhandvalue += dealer_cards[i].getValue();
+		}
+		this.checkDealerBust();
+	}
+
+	
+	/*
+	 * make sure that the value is under 17 
+	 * before the next hit
+	 */
+	public boolean dealerCanHit()
+	{
+		if(dealerhandvalue<17)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/*
 	 * return true/false based on whether the hand is a bust
 	 * 	by seeing if the value is over 21
@@ -211,6 +289,7 @@ public class Gameplay {
 		return false;	
 	}
 
+	
 	/*
 	 * checks to see if the player has lost
 	 * by going over 21 
@@ -223,6 +302,7 @@ public class Gameplay {
 		return false;	
 	}
 
+	
 	/*
 	 * check if the dealer's hand is a blackjack or not
 	 * needs to have 2 cards and a hand value of 21
@@ -236,6 +316,7 @@ public class Gameplay {
 		return false;
 	}
 
+	
 	/*
 	 * checks if the player's hand is a blackjack or not
 	 */
@@ -248,6 +329,7 @@ public class Gameplay {
 		return false;
 	}	
 
+	
 	/*
 	 * Checks the condition of the game and determines who has won 
 	 * after both can no longer or one of the two has won
@@ -263,6 +345,7 @@ public class Gameplay {
 			finalgame.addMessage();
 			playermoney += (2.5 * betamount);
 		}
+		
 		//if both people have blackjacks, these is a draw and the bet gets returned 
 		else if(playerBlackJack() && dealerBlackJack()) 
 		{
@@ -272,6 +355,7 @@ public class Gameplay {
 			finalgame.addMessage();
 			playermoney += betamount;
 		}
+		
 		//dealer has blackjack and player doesn't bust but doesn't have blackjack player doesn't gain any money 
 		else if(playerhandvalue < 21 && dealerBlackJack()) 
 		{
@@ -281,6 +365,7 @@ public class Gameplay {
 			finalgame.addMessage();
 
 		}
+		
 		//amount that is won is equivalent to twice the original amount
 		else if(playerhandvalue <=21 && dealerhandvalue < playerhandvalue)
 		{
@@ -299,6 +384,7 @@ public class Gameplay {
 			finalgame.setS(set);
 			finalgame.addMessage();
 		}
+		
 		//if player doesn't bust and dealer busts
 		else if(playerhandvalue <=21 && checkDealerBust())
 		{
@@ -307,6 +393,7 @@ public class Gameplay {
 			finalgame.setS(set);
 			finalgame.addMessage();
 		}
+		
 		//if player gets blackJack and dealer busts
 		else if(playerBlackJack() && checkDealerBust())
 		{
@@ -315,6 +402,7 @@ public class Gameplay {
 			finalgame.setS(set);
 			finalgame.addMessage();
 		}
+		
 		//if player gets blackJack and dealer busts
 		else if(playerBlackJack() && checkDealerBust())
 		{
@@ -340,10 +428,9 @@ public class Gameplay {
 			while(dealerCanHit()) 
 			{
 				dealerHit(deck);
-
 			}
-
 		}
+		
 		//if player already busted then victory goes to the dealer right away
 		else if( checkPlayerBust()==true)
 		{
@@ -353,6 +440,7 @@ public class Gameplay {
 			//			finalgame.setS(set);
 			//			finalgame.addMessage();
 		}
-
 	}
 }
+
+
